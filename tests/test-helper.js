d514eab890b6dc92ccfb960b00037eae32b841b9
davidkiama <davidkiama98@gmail.com>
2022-08-03 13:45:24 +0300

    Initial project setup

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'deezer/app';
+import config from 'deezer/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
