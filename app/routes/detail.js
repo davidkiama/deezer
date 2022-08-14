0a455c3fceeb86191b9ccced7f6562cb3966b136
davidkiama <davidkiama98@gmail.com>
2022-08-14 12:11:51 +0300

    Added index to top tracks component

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -11,7 +11,16 @@ export default class DetailRoute extends Route {
 
       const { albums, top, info } = await response.json();
 
-      return { ...albums, ...top, info };
+      //Add index to the top tracks
+
+      const [tops] = Object.values(top);
+
+      const mappedTops = tops.map((track, index) => {
+        track.index = index + 1;
+        return track;
+      });
+
+      return { ...albums, mappedTops, info };
     } catch (error) {}
   }
 }
